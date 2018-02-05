a92cf244242b49ef66ad3e5ea221069b7dd2af78
Bruno <mondelob14@gmail.com>
2018-02-05 17:20:53 +0000

    Don't call directly VGA routines and macros

--- Three/Kernel/main.asm
+++ Three/Kernel/main.asm
@@ -5,7 +5,8 @@
 format elf                       ; Specify output format ELF
 use32                            ; 32 Bit code
 
-include "graph/vga.inc"
+; include "graph/vga.inc"
+include "graph/term.inc"
 
 public kernel_main               ; Set the main function accessible for the
                                  ; multiboot
@@ -13,50 +14,9 @@ public kernel_main               ; Set the main function accessible for the
 section ".text"
 
 kernel_main:
-
-    mov bh, VGA_LIGHT_BLUE       ; Set BH register to the foreground color
-    mov bl, VGA_MAGENTA          ; Set BL register to the background color
-    call vga_color
-
-    mov bh, ah
-
-    mov bl, "H"
-    putchar bx, 0, 0
-
-    mov bl, "e"
-    putchar bx, 1, 0
-
-    mov bl, "l"
-    putchar bx, 2, 0
-
-    mov bl, "l"
-    putchar bx, 3, 0
-
-    mov bl, "o"
-    putchar bx, 4, 0
-
-    mov bl, ","
-    putchar bx, 5, 0
-
-    mov bl, " "
-    putchar bx, 6, 0
-
-    mov bl, "W"
-    putchar bx, 7, 0
-
-    mov bl, "o"
-    putchar bx, 8, 0
-
-    mov bl, "r"
-    putchar bx, 9, 0
-
-    mov bl, "l"
-    putchar bx, 10, 0
-
-    mov bl, "d"
-    putchar bx, 11, 0
-
-    mov bl, "!"
-    putchar bx, 12, 0
+    call init_term
+    sprint "H"
 
     ret
+
+hellomsg db 'Hello World!', 0
